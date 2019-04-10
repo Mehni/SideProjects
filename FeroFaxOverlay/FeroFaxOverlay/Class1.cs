12ec50e14799b61d7a95ac22cec15a76e24ee00f
Mehni <Mehni@users.noreply.github.com>
2019-04-10 22:32:43 +0200

    Add FeroFaxOverlay: Big Ugly X on Hostiles
    (useful for vision impaired)

---
+++ FeroFaxOverlay/FeroFaxOverlay/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+using UnityEngine;
+using Harmony;
+
+namespace FeroFaxOverlay
+{
+    [StaticConstructorOnStartup]
+    public static class HarmonyPatches
+    {
+        static HarmonyPatches()
+        {
+            HarmonyInstance harmony = HarmonyInstance.Create("Mehni.RimWorld.FeroFaxQuickAndDirtyOverlay");
+
+            harmony.Patch(AccessTools.Method(typeof(PawnUIOverlay), nameof(PawnUIOverlay.DrawPawnGUIOverlay)), null,
+                new HarmonyMethod(typeof(HarmonyPatches), nameof(AddABigUglyX_PostFix)), null);
+        }
+
+        private static void AddABigUglyX_PostFix(PawnUIOverlay __instance, Pawn ___pawn)
+        {
+            if (___pawn.RaceProps.Humanlike) return;
+
+            if (___pawn.Spawned && !___pawn.Map.fogGrid.IsFogged(___pawn.Position) &&___pawn.IsEnemyOfPlayer())
+                ___pawn.Map.overlayDrawer.DrawOverlay(___pawn, OverlayTypes.ForbiddenBig);
+        }
+
+        private static bool IsEnemyOfPlayer(this Pawn pawn) => pawn.InAggroMentalState || (pawn.Faction != null && pawn.Faction.HostileTo(Faction.OfPlayer));
+    }
+}
