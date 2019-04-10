28adb780a31bae5a193e6b9a49e11ddaf5321dde
Mehni <Mehni@users.noreply.github.com>
2019-04-10 22:46:08 +0200

    Add GenStepSettings: Settings window to alter XML value

---
+++ GenStepSettings/GenStepSettings/GenStepSettings.cs
@@ -0,0 +1,55 @@
+﻿using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace GenStepSettings
+{
+    using JetBrains.Annotations;
+
+    public class GenStepSettings : Mod
+    {
+        public readonly GenStepModSettings settings;
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            GUI.BeginGroup(inRect);
+            string value = "";
+            Widgets.IntEntry(inRect.RightPart(0.5f).BottomPart(0.5f), ref settings.count, ref value);
+            GUI.EndGroup();
+        }
+
+        public override string SettingsCategory() => "GenStepSettings";
+
+        public override void WriteSettings()
+        {
+            GenStep_Settlement genStep = (GenStep_Settlement)DefDatabase<GenStepDef>.GetNamed("Settlement").genStep;
+            genStep.count = settings.count;
+            base.WriteSettings();
+        }
+
+        public GenStepSettings(ModContentPack content) : base(content)
+        {
+            settings = GetSettings<GenStepModSettings>();
+        }
+    }
+
+    public class GenStepModSettings : ModSettings
+    {
+        public int count;
+
+        public override void ExposeData()
+        {
+            Scribe_Values.Look(ref count, "count", 1);
+        }
+    }
+
+    [StaticConstructorOnStartup]
+    public class SettingsImplementer
+    {
+        static SettingsImplementer()
+        {
+            GenStep_Settlement genStep = (GenStep_Settlement)DefDatabase<GenStepDef>.GetNamed("Settlement").genStep;
+            genStep.count = LoadedModManager.GetMod<GenStepSettings>().settings.count;
+        }
+    }
+}
