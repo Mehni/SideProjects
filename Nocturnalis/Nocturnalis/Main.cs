5641d25ca30759ab093e7dd289e5e06053b3ede8
Mehni <Mehni@users.noreply.github.com>
2019-04-10 11:11:40 +0200

    Add Nocturnalis: animal sleeping patterns

---
+++ Nocturnalis/Nocturnalis/Main.cs
@@ -0,0 +1,61 @@
+﻿using Harmony;
+using Verse;
+using RimWorld;
+
+namespace Nocturnalis
+{
+    using JetBrains.Annotations;
+
+    [UsedImplicitly]
+    [StaticConstructorOnStartup]
+    public static class HarmonyPatches
+    {
+        static HarmonyPatches()
+        {
+            HarmonyInstance harmony = HarmonyInstance.Create("mehni.rimworld.nocturnalis");
+
+            harmony.Patch(AccessTools.Method(typeof(JobGiver_GetRest), nameof(JobGiver_GetRest.GetPriority)),
+                new HarmonyMethod(typeof(HarmonyPatches), nameof(LetSleepingDogsWake_PreFix)));
+        }
+
+        private static bool LetSleepingDogsWake_PreFix(ref float __result, Pawn pawn)
+        {
+            if (!pawn.def.HasModExtension<SleepingPatterns>())
+                return true;
+
+            if (pawn.RaceProps.Humanlike)
+                return true;
+
+            if (pawn.timetable != null)
+                return true;
+
+            SleepingPatterns modExt = pawn.def.GetModExtension<SleepingPatterns>();
+
+            int hourOfDay = GenLocalDate.HourOfDay(pawn);
+
+            TimeAssignmentDef timeAssignmentDef = hourOfDay >= modExt.napTime[modExt.sleepingHabit].First && hourOfDay <= modExt.napTime[modExt.sleepingHabit].Second ? TimeAssignmentDefOf.Anything : TimeAssignmentDefOf.Sleep;
+
+            float curLevel = pawn.needs.rest.CurLevel;
+
+            if (timeAssignmentDef == TimeAssignmentDefOf.Anything)
+            {
+                if (curLevel < 0.3f)
+                    __result = 8f;
+
+                __result = 0f;
+                return false;
+            }
+
+            if (timeAssignmentDef == TimeAssignmentDefOf.Sleep)
+            {
+                if (curLevel < RestUtility.FallAsleepMaxLevel(pawn))
+                    __result = 8f;
+
+                __result = 0f;
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Nocturnalis/Nocturnalis/SleepingPatterns.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using RimWorld;
+using UnityEngine;
+
+namespace Nocturnalis
+{
+    public class SleepingPatterns : DefModExtension
+    {
+        public Dictionary<SleepingHabits, Pair<int, int>> napTime
+            = new Dictionary<SleepingHabits, Pair<int, int>>
+              {
+                  {SleepingHabits.Diurnal,    new Pair<int, int>(7 ,21)}, //wake at 7, sleep at 21 == 14 waking hours (or 10 sleeping hrs)
+                  {SleepingHabits.Nocturnal,  new Pair<int, int>(20, 7)},
+                  {SleepingHabits.Matutinal,  new Pair<int, int>(5, 13)},
+                  {SleepingHabits.Vespertine, new Pair<int, int>(17, 1)}
+              };
+
+        public SleepingHabits sleepingHabit;
+    }
+
+    public enum SleepingHabits
+    {
+        // ReSharper disable InconsistentNaming
+        Diurnal,
+        Nocturnal,
+        Matutinal,
+        Vespertine,
+        // ReSharper restore InconsistentNaming
+    }
+}
